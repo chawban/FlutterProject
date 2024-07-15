fc0fd94b876fa6eafcb663dd8d1c6bc5c52c5ef1
chawban <chawban@gmail.com>
2024-07-15 12:29:47 +0700

    Introduction to Flutter CCE 2567

---
+++ flutter_application_1/lib/Mainmenu.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MainMenu2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('asset/pig1.jpg'),
+              Image.asset('asset/pig2.jpg'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MenuItem(title: 'Item 3'),
+              MenuItem(title: 'Item 4.1'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MenuItem(title: 'Item 5'),
+              MenuItem(title: 'Item 6'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String title;
+
+  MenuItem({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.blue,
+      ),
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'Mainmenu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(title: 'สวัสดี Flutter'),
+      // home: MainMenu(),
+      home: MainMenu2(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.verified_user),
+      ),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Menu'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Number of columns
+            mainAxisSpacing: 10.0, // Spacing between rows
+            crossAxisSpacing: 10.0, // Spacing between columns
+            childAspectRatio: 1.0, // Aspect ratio of the grid items
+          ),
+          itemCount: 10, // Total number of items (2 columns * 3 rows)
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text(
+                  'Item ${index + 1}',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
